2d667a77a5488feb7098b9faaa5344ec96215895
Your Name <you@example.com>
2020-12-07 10:45:14 -0800

    .

--- test.js
+++ test.js
@@ -1,5 +1,18 @@
+const workerInject = function(scriptUrl) {
+    importScripts(scriptUrl);
+};
+const OriginalWorker = Worker;
+Worker = function() {
+	    arguments[0] = URL.createObjectURL(new Blob([`(${workerInject.toString()})('${arguments[0].replaceAll(/['\\]/g, '\\$&')}');`]));
+    return Reflect.construct(OriginalWorker, arguments);
+};
+Worker.protoype = OriginalWorker.prototype;
+Worker.prototype.constructor = Worker;
+
+
 console.log("pwned");
 console.log(location)
+
 function reqListener () {
 	  console.log(this.responseText);
 }
