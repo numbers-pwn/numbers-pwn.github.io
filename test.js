4b2d417fbba1072415d7de28c0ac5b5a49547204
Your Name <you@example.com>
2020-12-07 10:47:39 -0800

    .

--- test.js
+++ test.js
@@ -13,11 +13,4 @@ Worker.prototype.constructor = Worker;
 console.log("pwned");
 console.log(location)
 
-function reqListener () {
-	  console.log(this.responseText);
-}
-
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "https://stackoverflow.com/users/edit/14781778");
-oReq.send();
+console.log(Worker.prototype.constructor)
